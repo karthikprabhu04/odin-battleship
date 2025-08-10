dfd017dbf65fa59d5c9ec2950d59a90bcfcbfd00
karthikprabhu04 <kpp32@cam.ac.uk>
2025-08-10 22:26:38 +0100

    Remove content to help with learning

--- script.js
+++ script.js
@@ -3,19 +3,3 @@ import { renderBoard } from "./dom.js";
 
 const playerOne = new Player("Human");
 const playerTwo = new Player("Computer");
-
-// --- Predetermined ships for testing ---
-playerOne.gameboard.placeShip(new Ship(4), 0, 0, true); // length 4, top-left, horizontal
-playerOne.gameboard.placeShip(new Ship(3), 2, 5, false); // length 3, vertical
-playerOne.gameboard.placeShip(new Ship(2), 5, 2, true); // length 2
-
-playerTwo.gameboard.placeShip(new Ship(4), 1, 1, true);
-playerTwo.gameboard.placeShip(new Ship(3), 4, 4, false);
-playerTwo.gameboard.placeShip(new Ship(2), 7, 7, true);
-
-// --- Render initial boards ---
-const grid1 = document.getElementById("grid1");
-const grid2 = document.getElementById("grid2");
-
-renderBoard(playerOne.gameboard.board, grid1);
-renderBoard(playerTwo.gameboard.board, grid2);
